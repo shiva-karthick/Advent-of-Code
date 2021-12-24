dd6b6ac55f129b5bdb6b7c9edf387f5371c77593
Rani DilipKumar Shiva Shankar <shankaarshiva@gmail.com>
2021-12-25 01:12:06 +0800

    aoc-day20-small-edit

--- AOC_2021/Day-20-Trench-Map/day-20.py
+++ AOC_2021/Day-20-Trench-Map/day-20.py
@@ -2,6 +2,7 @@ filename = 'AOC_2021\Day-20-Trench-Map\input.txt'
 
 code, imagem = open(filename).read().split('\n\n')
 
+
 code, imagem = ['.#'.index(c) for c in code], imagem.splitlines()
 
 points = [(x, y) for y in range(-55, len(imagem)+55)
@@ -12,8 +13,7 @@ imagem = {(x, y): '.#'.index(c) for y, row in enumerate(imagem)
 
 
 def getbin(x, y, d):
-    return int(''.join(''.join('01'[imagem.get(
-        (n, y), d)] for n in range(x-1, x+2)) for y in range(y-1, y+2)), 2)
+    return int(''.join(''.join('01'[imagem.get((n, y), d)] for n in range(x-1, x+2)) for y in range(y-1, y+2)), 2)
 
 
 for i in range(2):
