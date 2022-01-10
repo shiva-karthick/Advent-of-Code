aec2a90223a99c3b20fa486fafdbcb53adbd9bcf
Rani DilipKumar Shiva Shankar <shankaarshiva@gmail.com>
2022-01-10 16:06:25 +0800

    small changes

--- AOC_2021/Day-18-Snailfish/day18.py
+++ AOC_2021/Day-18-Snailfish/day18.py
@@ -1,1 +1,83 @@
-# math.floor(self.numberRight/2), math.ceil(self.numberRight/2)
+from time import perf_counter as pfc
+from functools import reduce
+from itertools import permutations
+
+
+def read_puzzle(filename):
+    with open(filename) as f:
+        return f.readlines()
+
+
+def gen_flatlist(puzzle):
+    flatlist = []
+    for line in puzzle:
+        flatline, depth = [], 0
+        for c in line:
+            if c == '[':
+                depth += 1
+            elif c == ']':
+                depth -= 1
+            elif c.isdigit():
+                flatline.append([int(c), depth])
+        flatlist.append(flatline)
+    return flatlist
+
+
+def explode(x):
+    for i, ((num1, depth1), (num2, depth2)) in enumerate(zip(x, x[1:])):
+        if depth1 < 5 or depth1 != depth2:
+            continue
+        # the 2 if statements execute this --> "To explode a pair, the pair's left value is added to the rightmost regular number to the left of the exploding pair (if any), and the pair's right value is added to the leftmost regular number to the right of the exploding pair (if any)"
+        if i > 0:
+            x[i-1][0] += num1
+        if i < len(x)-2:  # meaning we are at the start of the list and there exists no element on the left
+            x[i+2][0] += num2
+        #
+        return True, x[:i] + [[0, depth1-1]] + x[i+2:]
+    return False, x
+
+
+def split(x):
+    for i, (num, depth) in enumerate(x):
+        if num < 10:
+            continue
+        down = num // 2
+        up = num - down
+        return True, x[:i] + [[down, depth+1], [up, depth+1]] + x[i+1:]
+    return False, x
+
+
+def add(a, b):
+    print(f" a + b = {a+b}")
+    x = [[num, depth+1] for num, depth in a + b]
+    while True:
+        change, x = explode(x)
+        if change:
+            continue
+        change, x = split(x)
+        if not change:
+            break
+    return x
+
+
+def magnitude(x):
+    while len(x) > 1:
+        for i, ((num1, depth1), (num2, depth2)) in enumerate(zip(x, x[1:])):
+            if depth1 != depth2:
+                continue
+            val = num1 * 3 + num2 * 2
+            x = x[:i]+[[val, depth1-1]]+x[i+2:]
+            break
+    return x[0][0]
+
+
+def solve(puzzle):
+    flatlist = gen_flatlist(puzzle)
+    part1 = magnitude(reduce(add, flatlist))
+    part2 = max(magnitude(add(a, b)) for a, b in permutations(flatlist, 2))
+    return part1, part2
+
+
+start = pfc()
+print(solve(read_puzzle('AOC_2021\Day-18-Snailfish\sample.txt')))
+print(pfc()-start)
--- AOC_2021/Day-25-Sea-Cucumber/Solution.java
+++ AOC_2021/Day-25-Sea-Cucumber/Solution.java
@@ -1,9 +1,108 @@
-import java.util.Arrays;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Solution{
 
-class Solution(){
-    public main static void (String[] args){
-        
+    private static char[][] grid;
+    private static final char EAST = '>';
+    private static final char SOUTH = 'v';
+    private static final char BLANK = '.';
+
+    public static void main (String[] args){
+        // Create a scanner class
+        System.out.println("Input > ");
+        Scanner reader = new Scanner(System.in);
+
+        List<String> input = new ArrayList<String>();
+        while (reader.hasNext()){
+            input.add(reader.nextLine());
+        }
+        reader.close();
+
+       grid = new char[input.size()][input.get(0).length()];
+       int r = 0;
+       for (String line : input){
+            grid[r] = line.toCharArray();
+            r += 1;
+       }
+
+       part1();
     }
 
+    public static void part1(){
+        
+        int steps = 0;
+        boolean movedEast = true;
+        boolean movedSouth = true;
+        
+        while (movedEast || movedSouth){
+            steps += 1;
+            movedEast = moveEast();
+            movedSouth = moveSouth();
+        }
+        System.out.printf("Number of steps = %d \n", steps);
+    }
+    
+    private static boolean moveEast(){
+        boolean moved = false;
+
+        for (int row = 0; row < grid.length; row += 1){
+            boolean moveOG = false;
+
+            for (int column = 0; column < grid[0].length; column += 1){
+                if (grid[row][column] == EAST){
+                    if (column <= grid[0].length - 2 && grid[row][column + 1] == BLANK){
+                        moved = true;
+                        if (column == 0){
+                            moveOG = true;
+                        }
+                        // Swap the places
+                        grid[row][column] = BLANK;
+                        grid[row][column + 1] = EAST;
+                        column += 1; 
+                    }else if (column == grid.length - 1 && grid[row][0] == BLANK && !moveOG){
+                        moved = true;
+                        grid[row][column] = BLANK;
+                        grid[row][0] = EAST;
+                    } 
+                }
+            }
+        }
+        return moved; 
+    }
+
+    private static boolean moveSouth(){
+       boolean moved = false;
+
+       for (int column = 0; column < grid[0].length; column += 1){
+            boolean movedOG = false;
+            for (int row = 0; row < grid.length; row += 1){
+                if (grid[row][column] == SOUTH){
+                    if (row <= grid.length - 2 && grid[row + 1][column] == BLANK){
+                        // Swap positions
+                        moved = true;
+                        if (row == 0){
+                            movedOG = true; // Ensure that at the end of the row, it doesn't move again,
+                            // resulting in duplicate moves
+                        }
+                        grid[row][column] = BLANK;
+                        grid[row + 1][column] = SOUTH;
+                        row += 1;
+                    } else if (row == grid.length - 1 && grid[0][column] == BLANK && !movedOG){
+                        moved = true;
+                        grid[row][column] = BLANK;
+                        grid[0][column] = SOUTH;
+                    }
+                }
+            }
+       }
+       return moved;
+    }
 }
+
+
+
+
+
